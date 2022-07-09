c2b8e291765060fe93d2957500527d03fabc4a2d
DevDenis29 <dev.tonerfish@seznam.cz>
2022-07-09 10:25:56 +0200

    cookie clicker

---
+++ cookie clicker/js/mainGame.js
@@ -0,0 +1,13 @@
+var cookie = 0;
+var cpc = 1;
+var upgrade = 0;
+var coin = 0;
+
+
+function cookie() {
+	cookie += cpc;
+	document.getElementById("cookieCount").innerHTML = cookie;
+	console.log(cookie);
+
+
+}
